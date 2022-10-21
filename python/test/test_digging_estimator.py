9393975b98ebbdfdbd3c300c350f6f040e95ae69
Ichak <ichakhari@gmail.com>
2022-10-21 15:26:56 +0200

    -> Creation des testes :     * Test si exception from API     * Mock API pour tester si == 48 (2 jour / 28metre /Granite)
    Comprehension du code :
        - Return Composition (nombre == total)
        - Composition built with 2 Teams (night/day)
    
     Changes to be committed:
    	new file:   .vscode/settings.json
    	modified:   python/test/__pycache__/test_digging_estimator.cpython-310-pytest-7.1.3.pyc
    	modified:   python/test/__pycache__/test_digging_estimator.cpython-39-pytest-7.1.3.pyc
    	modified:   python/test/test_digging_estimator.py

--- python/test/test_digging_estimator.py
+++ python/test/test_digging_estimator.py
@@ -1,9 +1,18 @@
+from unittest.mock import MagicMock
+import pytest
 from src.digging_estimator import DiggingEstimator
 
 
-def test_returns_as_doctor_Pockosky_says():
-    estimator = DiggingEstimator()
 
-    result = estimator.tunnel(28, 2, "granite")
+def test_should_be_Exception_From_Rock_API():
+    dig_estimator_tested = DiggingEstimator()
+    with pytest.raises(Exception):
+        dig_estimator_tested.tunnel(28, 2, "granite")
 
-    assert result.total == 48
+def test_should_be_48_for_2days_28_meters():
+    dig_estimator_tested = DiggingEstimator()
+    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+
+    nb_dwarves_needed = dig_estimator_tested.tunnel(28, 2, "granite")
+
+    assert nb_dwarves_needed.total == 48

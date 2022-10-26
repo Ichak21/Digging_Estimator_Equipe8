8abdb40f36b748a9595529c2182d36baf355bc28
Hocks <ichakhari@gmail.com>
2022-10-26 12:38:27 +0200

    Your branch is up to date with 'origin/master'.
    Fin refactoring :
    - ajout de fonction a team et composition
    - typage
    
     Changes to be committed:
    	modified:   python/src/__pycache__/digging_estimator.cpython-310.pyc
    	modified:   python/src/digging_estimator.py

--- python/src/digging_estimator.py
+++ python/src/digging_estimator.py
@@ -2,6 +2,10 @@ import math
 from enum import Enum
 
 
+def average_per_day(tunnel_length: int, digging_days: int):
+    return math.floor(tunnel_length / digging_days)
+
+
 class Shift(Enum):
     NIGHT = 0
     DAY = 1
@@ -16,6 +20,15 @@ class InvalidFormatException(Exception):
 
 
 class Team:
+    miners: int
+    healers: int
+    smithies: int
+    lighters: int
+    inn_keepers: int
+    guards: int
+    guard_managers: int
+    washers: int
+
     def __init__(self):
         self.miners = 0
         self.healers = 0
@@ -44,42 +57,117 @@ class Team:
             self.washers = math.ceil((self.miners + self.healers + self.smithies + self.inn_keepers +
                                       self.lighters + self.guards + self.guard_managers) / 10.0)
 
-    def set_guards(self):
-        pass
+    def night_updater_for_all_guard_and_washer(self):
+        previous_washers: int = 0
+        previous_guards: int = 0
+        previous_guard_manager: int = 0
 
-    def set_guard_managers(self):
-        pass
+        while not (previous_washers == self.washers and previous_guards == self.guards and previous_guard_manager == self.guard_managers):
+
+            previous_washers = self.washers
+            previous_guards = self.guards
+            previous_guard_manager = self.guard_managers
+
+            self.washers = math.ceil((self.miners + self.healers + self.smithies +
+                                     self.inn_keepers + self.lighters + self.guards + self.guard_managers) / 10.0)
+            self.guards = math.ceil(
+                (self.healers + self.miners + self.smithies + self.lighters + self.washers) / 3.0)
+            self.guard_managers = math.ceil((self.guards) / 3.0)
+
+    def get_head_count(self):
+        return self.miners + self.washers + self.healers + self.smithies + self.inn_keepers + self.guards + self.guard_managers + self.lighters
 
 
 class TeamComposition:
+    MINER_INCREMENTOR: int = 1
+
     def __init__(self):
         self.day_team: Team = Team()
         self.night_team: Team = Team()
-        self.head_count = 0
+        self.head_count: int = 0
+
+    def define_miners_needed(self, distances_per_dwarf_numbers: int, tunnel_length: int, digging_days: int, max_dig_per_shift: int):
+        for dig_distance in range(0, len(distances_per_dwarf_numbers) - 1):
+            if distances_per_dwarf_numbers[dig_distance] < average_per_day(tunnel_length, digging_days):
+                self.day_team.miners += self.MINER_INCREMENTOR
+
+            if distances_per_dwarf_numbers[dig_distance] + max_dig_per_shift < average_per_day(tunnel_length, digging_days):
+                self.night_team.miners += self.MINER_INCREMENTOR
+
+        return self
+
+    def day_shift_handling(self):
+        day_shift: Team = self.day_team
+
+        if day_shift.miners > 0:
+            day_shift.healers += 1
+            day_shift.smithies += 2
+            day_shift.set_inn_keepers(Shift.DAY)
+            day_shift.set_washers(Shift.DAY)
+
+        self.update_head_count()
+        return self
+
+    def night_shift_handling(self):
+        night_shift: Team = self.night_team
+
+        if night_shift.miners > 0:
+            night_shift.healers += 1
+            night_shift.smithies += 2
+            night_shift.lighters = night_shift.miners + 1
+            night_shift.set_inn_keepers(Shift.NIGHT)
+            night_shift.set_washers(Shift.NIGHT)
+            night_shift.night_updater_for_all_guard_and_washer()
+
+        self.update_head_count()
+        return self
+
+    def update_head_count(self):
+        self.head_count = self.day_team.get_head_count() + self.night_team.get_head_count()
+
+    def get_head_count(self):
+        return self.head_count
 
 
 class DiggingEstimator:
-    MAX_ROTATION = 2
-    LIMIT_LENGTH = 0
-    LIMIT_DAYS = 0
-    MINER_INCREMENTOR = 1
+    MAX_ROTATION: int = 2
+    LIMIT_LENGTH: int = 0
+    LIMIT_DAYS: int = 0
 
-    def digging_project_is_possible(self, tunnel_length, digging_days, max_dig_per_days):
+    def tunnel(self, tunnel_length, digging_days, rock_type):
+
+        distances_per_dwarf_numbers: list = self.capacity_calculate(rock_type)[
+            0]
+        max_dig_per_shift: int = self.capacity_calculate(rock_type)[1]
+        max_dig_per_day: int = self.capacity_calculate(rock_type)[2]
+
+        self.digging_project_is_possible(
+            tunnel_length, digging_days, max_dig_per_day)
+
+        composition_for_project: TeamComposition = TeamComposition()
+        composition_for_project.define_miners_needed(
+            distances_per_dwarf_numbers, tunnel_length, digging_days, max_dig_per_shift)
+        composition_for_project.day_shift_handling()
+        composition_for_project.night_shift_handling()
+
+        return composition_for_project
+
+    def digging_project_is_possible(self, tunnel_length, digging_days, max_dig_per_days: int):
         if not(isinstance(tunnel_length, int) and tunnel_length > 0 and isinstance(digging_days, int) and digging_days > 0):
             raise InvalidFormatException()
 
         if math.floor(tunnel_length / digging_days) > max_dig_per_days:
             raise TunnelTooLongForDelayException()
 
-    def capacity_calculate(self, rock_type):
-        dig_per_dwarf_numbers = self.get(rock_type)
-        max_dig_per_shift = dig_per_dwarf_numbers[len(
+    def capacity_calculate(self, rock_type: str):
+        dig_per_dwarf_numbers: list = self.get(rock_type)
+        max_dig_per_shift: float = dig_per_dwarf_numbers[len(
             dig_per_dwarf_numbers) - 1]
-        max_dig_per_days = self.MAX_ROTATION * max_dig_per_shift
+        max_dig_per_days: float = self.MAX_ROTATION * max_dig_per_shift
 
         return [dig_per_dwarf_numbers, max_dig_per_shift, max_dig_per_days]
 
-    def get(self, rock_type):
+    def get(self, rock_type: str):
         # for example for granite it returns [0, 3, 5.5, 7]
         # if you put 0 dwarf, you dig 0m / d / team
         # if you put 1 dwarf, you dig 3m / d / team
@@ -88,78 +176,3 @@ class DiggingEstimator:
         url = "dtp://research.vin.co/digging-rate/" + rock_type
         print("Trying to fetch" + url)
         raise Exception("Does not work in test mode")
-
-    def average_per_day(self, lg, dys):
-        return math.floor(lg / dys)
-
-    def day_shift_handling(self, composition):
-        day_shift = composition.day_team
-
-        # DAY TIME
-        if day_shift.miners > 0:
-            day_shift.healers += 1
-            day_shift.smithies += 2
-            day_shift.set_inn_keepers(Shift.DAY)
-            day_shift.set_washers(Shift.DAY)
-
-        composition.head_count = self.day_shift_addition(day_shift)
-
-        return composition.head_count
-
-    def night_shift_handling(self, composition):
-        self.composition = TeamComposition()
-        night_shift = composition.night_team
-        # NIGHT TIME
-        if night_shift.miners > 0:
-            night_shift.healers += 1
-            night_shift.smithies += 2
-            night_shift.lighters = night_shift.miners + 1
-            night_shift.inn_keepers = math.ceil(
-                (night_shift.miners + night_shift.healers + night_shift.smithies + night_shift.lighters) / 4.0) * 4
-            while True:
-                old_washers = night_shift.washers
-                old_guards = night_shift.guards
-                old_chief_guard = night_shift.guard_managers
-
-                night_shift.washers = math.ceil((night_shift.miners + night_shift.healers + night_shift.smithies +
-                                                night_shift.inn_keepers + night_shift.lighters + night_shift.guards + night_shift.guard_managers) / 10.0)
-                night_shift.guards = math.ceil(
-                    (night_shift.healers + night_shift.miners + night_shift.smithies + night_shift.lighters + night_shift.washers) / 3.0)
-                night_shift.guard_managers = math.ceil(
-                    (night_shift.guards) / 3.0)
-
-                if old_washers == night_shift.washers and old_guards == night_shift.guards and old_chief_guard == night_shift.guard_managers:
-                    break
-        composition.head_count = self.night_shift_addition(night_shift)
-        return composition.head_count
-
-    def day_shift_addition(self, team):
-        self.team = Team()
-        return team.miners + team.washers + team.healers + team.smithies + team.inn_keepers
-
-    def night_shift_addition(self, team):
-        self.team = Team()
-        return team.miners + team.washers + team.healers + team.smithies + team.inn_keepers + team.guards + team.guard_managers + team.lighters
-
-    def tunnel(self, tunnel_length, digging_days, rock_type):
-        distances_per_dwarf_numbers = self.capacity_calculate(rock_type)[0]
-        max_dig_per_shift = self.capacity_calculate(rock_type)[1]
-        max_dig_per_day = self.capacity_calculate(rock_type)[2]
-
-        self.digging_project_is_possible(
-            tunnel_length, digging_days, max_dig_per_day)
-
-        composition = TeamComposition()
-
-        # Miners
-        for dig_distance in range(0, len(distances_per_dwarf_numbers) - 1):
-            if distances_per_dwarf_numbers[dig_distance] < self.average_per_day(tunnel_length, digging_days):
-                composition.day_team.miners += self.MINER_INCREMENTOR
-
-            if distances_per_dwarf_numbers[dig_distance] + max_dig_per_shift < self.average_per_day(tunnel_length, digging_days):
-                composition.night_team.miners += self.MINER_INCREMENTOR
-
-        composition.head_count = self.day_shift_handling(
-            composition) + self.night_shift_handling(composition)
-
-        return composition

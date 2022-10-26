744bed814f1681a046da95b62634a8476132e20c
Hocks <ichakhari@gmail.com>
2022-10-26 11:39:28 +0200

    Your branch is up to date with 'origin/master'.
    Refactor V3 :
    - concaténation du refactoring 2eme branch(calcul) & master (gestion dexception)
    - ajout de setter a Team
    
     Changes to be committed:
    	modified:   python/src/__pycache__/digging_estimator.cpython-310.pyc
    	modified:   python/src/digging_estimator.py
    	modified:   python/test/__pycache__/test_digging_estimator.cpython-310-pytest-7.1.3.pyc
    	modified:   python/test/test_digging_estimator.py

--- python/src/digging_estimator.py
+++ python/src/digging_estimator.py
@@ -1,4 +1,10 @@
 import math
+from enum import Enum
+
+
+class Shift(Enum):
+    NIGHT = 0
+    DAY = 1
 
 
 class TunnelTooLongForDelayException(Exception):
@@ -20,94 +26,58 @@ class Team:
         self.guard_managers = 0
         self.washers = 0
 
+    def set_inn_keepers(self, night_or_day: Shift):
+        if night_or_day == Shift.DAY:
+            self.inn_keepers = math.ceil(
+                (self.miners + self.healers + self.smithies) / 4.0) * 4
+
+        elif night_or_day == Shift.NIGHT:
+            self.inn_keepers = math.ceil((self.miners + self.healers +
+                                          self.smithies + self.lighters) / 4.0) * 4
+
+    def set_washers(self, night_or_day: Shift):
+        if night_or_day == Shift.DAY:
+            self.washers = math.ceil((self.miners + self.healers +
+                                      self.smithies + self.inn_keepers) / 10.0)
+
+        elif night_or_day == Shift.NIGHT:
+            self.washers = math.ceil((self.miners + self.healers + self.smithies + self.inn_keepers +
+                                      self.lighters + self.guards + self.guard_managers) / 10.0)
+
+    def set_guards(self):
+        pass
+
+    def set_guard_managers(self):
+        pass
+
 
 class TeamComposition:
     def __init__(self):
         self.day_team: Team = Team()
         self.night_team: Team = Team()
-        self.total = 0
+        self.head_count = 0
 
 
 class DiggingEstimator:
-    def digging_project_is_possible(self, tunnel_length, digging_days, max_capacity_dig_per_days):
+    MAX_ROTATION = 2
+    LIMIT_LENGTH = 0
+    LIMIT_DAYS = 0
+    MINER_INCREMENTOR = 1
+
+    def digging_project_is_possible(self, tunnel_length, digging_days, max_dig_per_days):
         if not(isinstance(tunnel_length, int) and tunnel_length > 0 and isinstance(digging_days, int) and digging_days > 0):
             raise InvalidFormatException()
 
-        if math.floor(tunnel_length / digging_days) > max_capacity_dig_per_days:
+        if math.floor(tunnel_length / digging_days) > max_dig_per_days:
             raise TunnelTooLongForDelayException()
 
     def capacity_calculate(self, rock_type):
         dig_per_dwarf_numbers = self.get(rock_type)
-        max_capacity_dig_per_shift = dig_per_dwarf_numbers[len(
+        max_dig_per_shift = dig_per_dwarf_numbers[len(
             dig_per_dwarf_numbers) - 1]
-        max_capacity_dig_per_days = 2 * max_capacity_dig_per_shift
+        max_dig_per_days = self.MAX_ROTATION * max_dig_per_shift
 
-        return [dig_per_dwarf_numbers, max_capacity_dig_per_shift, max_capacity_dig_per_days]
-
-    def tunnel(self, length, days, rock_type):
-        dig_per_dwarf_numbers = self.capacity_calculate(rock_type)[0]
-        max_dig_per_rotation = self.capacity_calculate(rock_type)[1]
-        max_dig_per_day = self.capacity_calculate(rock_type)[2]
-
-        self.digging_project_is_possible(length, days, max_dig_per_day)
-
-        composition = TeamComposition()
-
-        # Miners
-        for i in range(0, len(dig_per_dwarf_numbers)-1):
-            if dig_per_dwarf_numbers[i] < math.floor(length / days):
-                composition.day_team.miners += 1
-
-        if math.floor(length / days) > max_dig_per_rotation:
-            for i in range(0, len(dig_per_dwarf_numbers) - 1):
-                if dig_per_dwarf_numbers[i] + max_dig_per_rotation < math.floor(length / days):
-                    composition.night_team.miners += 1
-
-        dt = composition.day_team
-        nt = composition.night_team
-
-        if dt.miners > 0:
-            dt.healers += 1
-            dt.smithies += 1
-            dt.smithies += 1
-
-        if nt.miners > 0:
-            nt.healers += 1
-            nt.smithies += 1
-            nt.smithies += 1
-
-        if nt.miners > 0:
-            nt.lighters = nt.miners + 1
-
-        if dt.miners > 0:
-            dt.inn_keepers = math.ceil(
-                (dt.miners + dt.healers + dt.smithies) / 4.0) * 4
-            dt.washers = math.ceil(
-                (dt.miners + dt.healers + dt.smithies + dt.inn_keepers) / 10.0)
-
-        if nt.miners > 0:
-            nt.inn_keepers = math.ceil(
-                (nt.miners + nt.healers + nt.smithies + nt.lighters) / 4.0) * 4
-
-        while True:
-            old_washers = nt.washers
-            old_guards = nt.guards
-            old_chief_guard = nt.guard_managers
-
-            nt.washers = math.ceil((nt.miners + nt.healers + nt.smithies +
-                                   nt.inn_keepers + nt.lighters + nt.guards + nt.guard_managers) / 10.0)
-            nt.guards = math.ceil(
-                (nt.healers + nt.miners + nt.smithies + nt.lighters + nt.washers) / 3.0)
-            nt.guard_managers = math.ceil((nt.guards) / 3.0)
-
-            if old_washers == nt.washers and old_guards == nt.guards and old_chief_guard == nt.guard_managers:
-                break
-
-        composition.total = dt.miners + dt.washers + dt.healers + dt.smithies + dt.inn_keepers + nt.miners + \
-            nt.washers + nt.healers + nt.smithies + nt.inn_keepers + \
-            nt.guards + nt.guard_managers + nt.lighters
-
-        return composition
+        return [dig_per_dwarf_numbers, max_dig_per_shift, max_dig_per_days]
 
     def get(self, rock_type):
         # for example for granite it returns [0, 3, 5.5, 7]
@@ -118,3 +88,78 @@ class DiggingEstimator:
         url = "dtp://research.vin.co/digging-rate/" + rock_type
         print("Trying to fetch" + url)
         raise Exception("Does not work in test mode")
+
+    def average_per_day(self, lg, dys):
+        return math.floor(lg / dys)
+
+    def day_shift_handling(self, composition):
+        day_shift = composition.day_team
+
+        # DAY TIME
+        if day_shift.miners > 0:
+            day_shift.healers += 1
+            day_shift.smithies += 2
+            day_shift.set_inn_keepers(Shift.DAY)
+            day_shift.set_washers(Shift.DAY)
+
+        composition.head_count = self.day_shift_addition(day_shift)
+
+        return composition.head_count
+
+    def night_shift_handling(self, composition):
+        self.composition = TeamComposition()
+        night_shift = composition.night_team
+        # NIGHT TIME
+        if night_shift.miners > 0:
+            night_shift.healers += 1
+            night_shift.smithies += 2
+            night_shift.lighters = night_shift.miners + 1
+            night_shift.inn_keepers = math.ceil(
+                (night_shift.miners + night_shift.healers + night_shift.smithies + night_shift.lighters) / 4.0) * 4
+            while True:
+                old_washers = night_shift.washers
+                old_guards = night_shift.guards
+                old_chief_guard = night_shift.guard_managers
+
+                night_shift.washers = math.ceil((night_shift.miners + night_shift.healers + night_shift.smithies +
+                                                night_shift.inn_keepers + night_shift.lighters + night_shift.guards + night_shift.guard_managers) / 10.0)
+                night_shift.guards = math.ceil(
+                    (night_shift.healers + night_shift.miners + night_shift.smithies + night_shift.lighters + night_shift.washers) / 3.0)
+                night_shift.guard_managers = math.ceil(
+                    (night_shift.guards) / 3.0)
+
+                if old_washers == night_shift.washers and old_guards == night_shift.guards and old_chief_guard == night_shift.guard_managers:
+                    break
+        composition.head_count = self.night_shift_addition(night_shift)
+        return composition.head_count
+
+    def day_shift_addition(self, team):
+        self.team = Team()
+        return team.miners + team.washers + team.healers + team.smithies + team.inn_keepers
+
+    def night_shift_addition(self, team):
+        self.team = Team()
+        return team.miners + team.washers + team.healers + team.smithies + team.inn_keepers + team.guards + team.guard_managers + team.lighters
+
+    def tunnel(self, tunnel_length, digging_days, rock_type):
+        distances_per_dwarf_numbers = self.capacity_calculate(rock_type)[0]
+        max_dig_per_shift = self.capacity_calculate(rock_type)[1]
+        max_dig_per_day = self.capacity_calculate(rock_type)[2]
+
+        self.digging_project_is_possible(
+            tunnel_length, digging_days, max_dig_per_day)
+
+        composition = TeamComposition()
+
+        # Miners
+        for dig_distance in range(0, len(distances_per_dwarf_numbers) - 1):
+            if distances_per_dwarf_numbers[dig_distance] < self.average_per_day(tunnel_length, digging_days):
+                composition.day_team.miners += self.MINER_INCREMENTOR
+
+            if distances_per_dwarf_numbers[dig_distance] + max_dig_per_shift < self.average_per_day(tunnel_length, digging_days):
+                composition.night_team.miners += self.MINER_INCREMENTOR
+
+        composition.head_count = self.day_shift_handling(
+            composition) + self.night_shift_handling(composition)
+
+        return composition
--- python/test/test_digging_estimator.py
+++ python/test/test_digging_estimator.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock
 import pytest
 from src.digging_estimator import TunnelTooLongForDelayException
 from src.digging_estimator import InvalidFormatException
-from src.digging_estimator import DiggingEstimator   
+from src.digging_estimator import DiggingEstimator
 
 
 def test_should_be_Exception_From_Rock_API():
@@ -11,9 +11,10 @@ def test_should_be_Exception_From_Rock_API():
     with pytest.raises(Exception):
         dig_estimator_tested.tunnel(28, 2, "granite")
 
+
 def test_should_be_Exception_format_for_input():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     with pytest.raises(InvalidFormatException):
         dig_estimator_tested.tunnel(5.5, 2, "granite")
@@ -21,109 +22,123 @@ def test_should_be_Exception_format_for_input():
 
 def test_should_be_Exception_tunnel_too_long():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
-    
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
+
     with pytest.raises(TunnelTooLongForDelayException):
         dig_estimator_tested.tunnel(100, 2, "granite")
 
+
 def test_1dwarve_should_dig_3m_by_day_in_granite():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(3, 1, "granite")
 
     assert nb_dwarves_needed.day_team.miners == 1
 
+
 def test_1shift_1dwarve_should_9_total_dwarves():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(3, 1, "granite")
 
-    assert nb_dwarves_needed.total == 9
+    assert nb_dwarves_needed.head_count == 9
+
 
 def test_3dwarve_should_dig_7m_by_day_in_granite():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(7, 1, "granite")
 
     assert nb_dwarves_needed.day_team.miners == 3
 
+
 def test_should_be_48_dwarves_for_2days_28_meters():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(28, 2, "granite")
 
-    assert nb_dwarves_needed.total == 48
+    assert nb_dwarves_needed.head_count == 48
+
 
 def test_possible_to_have_2shift_in_one_day():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(11, 1, "granite")
 
     assert nb_dwarves_needed.day_team.miners > 0
     assert nb_dwarves_needed.night_team.miners > 0
 
+
 def test_we_must_have_2_smithies_by_shift():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(21, 2, "granite")
 
     assert nb_dwarves_needed.day_team.smithies == 2
     assert nb_dwarves_needed.night_team.smithies == 2
 
+
 def test_we_must_have_1_healers_by_shift():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(14, 1, "granite")
 
     assert nb_dwarves_needed.day_team.healers == 1
     assert nb_dwarves_needed.night_team.healers == 1
 
+
 def test_we_must_have_1_lighter_by_miners_and_1_lighter_for_camp_for_nightshift():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(17, 2, "granite")
 
     assert nb_dwarves_needed.day_team.lighters == 0
-    assert nb_dwarves_needed.night_team.lighters ==  nb_dwarves_needed.night_team.miners+1
+    assert nb_dwarves_needed.night_team.lighters == nb_dwarves_needed.night_team.miners+1
+
 
 def test_we_must_have_1_innkeepers_by_4_dwaves_in_miners_smithies_healers_lighters():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(28, 2, "granite")
-    
-    assert nb_dwarves_needed.day_team.inn_keepers + nb_dwarves_needed.night_team.inn_keepers == 20
+
+    assert nb_dwarves_needed.day_team.inn_keepers + \
+        nb_dwarves_needed.night_team.inn_keepers == 20
+
 
 def test_we_must_have_1_guards_by_3_dwaves_exclude_innkeeper():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(28, 2, "granite")
-    
-    assert nb_dwarves_needed.day_team.guards + nb_dwarves_needed.night_team.guards == 5
+
+    assert nb_dwarves_needed.day_team.guards + \
+        nb_dwarves_needed.night_team.guards == 5
+
 
 def test_we_must_have_1_guardsManager_by_3_guards():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(28, 2, "granite")
-    
-    assert nb_dwarves_needed.day_team.guard_managers + nb_dwarves_needed.night_team.guard_managers == 2
+
+    assert nb_dwarves_needed.day_team.guard_managers + \
+        nb_dwarves_needed.night_team.guard_managers == 2
+
 
 def test_we_must_have_1_washer_by_10_dwarves():
     dig_estimator_tested = DiggingEstimator()
-    dig_estimator_tested.get = MagicMock(return_value = [0, 3, 5.5, 7])
+    dig_estimator_tested.get = MagicMock(return_value=[0, 3, 5.5, 7])
 
     nb_dwarves_needed = dig_estimator_tested.tunnel(28, 2, "granite")
-    
-    assert nb_dwarves_needed.day_team.washers + nb_dwarves_needed.night_team.washers == 5
 
-
+    assert nb_dwarves_needed.day_team.washers + \
+        nb_dwarves_needed.night_team.washers == 5
